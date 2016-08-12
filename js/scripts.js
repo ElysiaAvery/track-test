1940307bf8c0ca88e5f0f5697de821b43f1c2202
Elysia Nason <elysia.avery@gmail.com>
2016-08-12 09:03:08 -0700

    now has a point system with branching with the use of jQuery and JavaScript

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,19 @@
 $(document).ready(function() {
-  $("form").submit(function() {
-    var slide = $("#question1").is(":hidden");
+  $("form").submit(function(event) {
+    event.preventDefault();
+    var question1 = parseInt($("#question1").val());
+    var question2 = parseInt($("#question2").val());
+    var question3 = parseInt($("#question1").val());
+    var question4 = parseInt($("#question2").val());
+    var question5 = parseInt($("#question1").val());
+    var counter = question1 + question2 + question3 + question4 + question5;
+    if (counter === 1 && counter < 6) {
+      $("#answer1").show();
+    } else if (counter === 6 && counter < 11) {
+      $("#answer2").show();
+    } else if (counter === 11 && counter < 16) {
+      $("#answer3").show();
+    }
 
-    $("img").slideToggle();
-    if(":visible")
-      $("#question2").slideToggle("slow") && $("#question3").slideToggle("slow") &&
-      $("#question4").slideToggle("slow") &&
-      $("#question5").slideToggle("slow");
   });
 });
