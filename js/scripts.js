051c65ce0cf34011d655f4d2fb5e119aaa82a7ff
Elysia Nason <elysia.avery@gmail.com>
2016-08-12 14:11:37 -0700

    have encountered more bugs

--- js/scripts.js
+++ js/scripts.js
@@ -11,56 +11,65 @@ $(document).ready(function() {
     var design = 0;
     var drupal = 0;
 
-    if(q1Answer === "question1option1") {
-      ruby += 1;
-    } else if (q1Answer === "question1option2"){
+    if(q1Answer === "question1option1"){
       design +=1;
-    }else if (q1Answer === "question1option3"){
-      drupal +=1;
-
-    if(q2Answer === "question2option1") {
-      ruby += 2;
-    } else if (q2Answer === "question2option2"){
-      design +=2;
-    } else if (q2Answer === "question2option3"){
+    }
+    else if (q1Answer === "question1option2"){
       drupal +=2;
-
-    if(q3Answer === "question3option1") {
-      ruby += 2;
-    } else if (q3Answer === "question3option2"){
-      design +=2;
-    } else if (q3Answer === "question3option3"){
+    }
+    else if (q1Answer === "question1option3"){
+      ruby +=3;
+    }
+    if(q2Answer === "question2option1"){
+      design +=1;
+    }
+    else if (q2Answer === "question2option2"){
       drupal +=2;
-
-    if(q4Answer === "question4option1") {
-      ruby += 3;
-    } else if (q4Answer === "question4option2"){
-      design += 3;
-      ruby -=2;
-    } else if (q4Answer === "question4option3"){
-      design += 3;
-
-    if(q4Answer === "question5option1") {
-      ruby += 3;
-      design -= 1;
-    } else if (q4Answer === "question5option2"){
-      design += 3;
-      ruby -=2;
-    } else if (q4Answer === "question5option3"){
-      ruby += 3;
-
+    }
+    else if (q2Answer === "question2option3"){
+      ruby +=3;
+    }
+    if(q3Answer === "question3option1"){
+      design +=1;
+    }
+    else if (q3Answer === "question3option2"){
+      drupal +=2;
+    }
+    else if (q3Answer === "question3option3"){
+      ruby +=3;
+    }
+    if(q4Answer === "question4option1"){
+      design +=1;
+    }
+    else if (q4Answer === "question4option2"){
+      drupal +=2;
+    }
+    else if (q4Answer === "question4option3"){
+      ruby +=3;
+    }
+    if(q4Answer === "question5option1"){
+      design +=1;
+    }
+    else if (q4Answer === "question5option2"){
+      drupal +=2;
+    }
+    else if (q4Answer === "question5option3"){
+      ruby +=3;
+    }
     var result = "";
 
     if((ruby>drupal) && (ruby>design)){
       result = "<h2>Ruby</h2>";
-    } else if ((drupal>ruby) && (drupal>design)){
+    }
+    else if ((drupal>ruby) && (drupal>design)){
       result = "<h2>drupal</h2>";
-    } else if ((design>ruby) && (design>drupal)){
+    }
+    else if ((design>ruby) && (design>drupal)){
       result = "<h2>design</h2>";
-    } else {
+    }
+    else {
       result = "<h2>You should read up on all of the programming languages and decide!</h2>";
     }
-      $("#results").html(result);
-    };
+    $("#results").html(result);
   });
 });
