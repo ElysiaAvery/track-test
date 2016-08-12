d83b86137f7393bf1437c2903a3acee03ce275b2
Elysia Nason <elysia.avery@gmail.com>
2016-08-12 09:34:20 -0700

    have more variables to accomodate for a value of 15 possible answers.

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,30 @@
 $(document).ready(function() {
   $("form").submit(function(event) {
     event.preventDefault();
-    var question1 = parseInt($("#question1").val());
-    var question2 = parseInt($("#question2").val());
-    var question3 = parseInt($("#question1").val());
-    var question4 = parseInt($("#question2").val());
-    var question5 = parseInt($("#question1").val());
-    var counter = question1 + question2 + question3 + question4 + question5;
-    if (counter === 1 && counter < 6) {
-      $("#answer1").show();
-    } else if (counter === 6 && counter < 11) {
-      $("#answer2").show();
-    } else if (counter === 11 && counter < 16) {
-      $("#answer3").show();
+    var question1option1 = parseInt($("#question1option1").val());
+    var question1option2 = parseInt($("#question1option2").val());
+    var question1option3 = parseInt($("#question1option3").val());
+    var question2option1 = parseInt($("#question2option1").val());
+    var question2option2 = parseInt($("#question2option2").val());
+    var question2option3 = parseInt($("#question2option3").val());
+    var question3option1 = parseInt($("#question3option1").val());
+    var question3option2 = parseInt($("#question3option2").val());
+    var question3option3 = parseInt($("#question3option3").val());
+    var question4option1 = parseInt($("#question4option1").val());
+    var question4option2 = parseInt($("#question4option2").val());
+    var question4option3 = parseInt($("#question4option3").val());
+    var question5option1 = parseInt($("#question5option1").val());
+    var question5option2 = parseInt($("#question5option2").val());
+    var question5option3 = parseInt($("#question5option3").val());
+    
+    if (question1option1 + question2option1 + question3option1 + question4option1 + question5option1) {
+      $("#answer").text("It looks like you should pursue a track of Ruby and Rails!");
+    }
+    else if (question1option2 + question2option2 + question3option2 + question4option2 + question5option2) {
+      $("#answer").text("You should consider the CSS and Design track!");
+    }
+    else if (question1option3 + question2option3 + question3option3 + question4option3 + question5option3) {
+      $("#answer").text("The Java and Android tracks sound perfect for you!");
     }
 
   });
